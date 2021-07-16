a816da45c3452a292091b058e7ebf64eee35593d
sujay-19 <85878065+sujay-19@users.noreply.github.com>
2021-07-16 17:48:04 +0530

    Add files via upload

---
+++ js/cannonball.js
@@ -0,0 +1,31 @@
+class Cannonball {
+    constructor(x, y) {
+      var options = {
+        restitution:0.9,
+        friction:1.0,
+        density:1.3,
+        isStatic:true
+      };
+      this.image = loadImage("assets/cannonball.png");
+      this.r=40
+      this.body = Bodies.circle(x, y,this.r,options);
+      World.add(world, this.body);
+    }
+    shoot() {
+        var velocity=p5.Vector.fromAngle(cannon.angle)
+        velocity.mult(20)
+        Matter.Body.setStatic(this.body,false)
+        Matter.Body.setVelocity(this.body,{x:velocity.x,y:velocity.y})
+    }
+    display() {
+      var pos = this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      imageMode(CENTER);
+      image(this.image, 0,0,this.r,this.r);
+      pop();
+    }
+  }
+  
